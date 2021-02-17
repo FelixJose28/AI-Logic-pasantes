6975a5f602c3df49ca3f889171600f6f4e3b9636
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-02-17 00:28:02 -0400

    container div and main_container working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const toggle = document.querySelector('.toggle_btn');
 const sidebar = document.querySelector('#sidebar');
+const main = document.querySelector('#main');
 toggle.addEventListener("click",function(){
     toggleMove();
 });
 
 function toggleMove() {
     sidebar.classList.toggle('active');
+    // main.classList.toggle('resaze')
 }
