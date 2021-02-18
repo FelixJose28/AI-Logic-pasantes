cdf80a2ab21c7496e5846c0ed204eabda8e5fb1b
Gabriel Gil Geronimo <sr.gabigil@gmail.com>
2021-02-17 22:48:21 -0400

    mejoras al sibarmenu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-const toggle = document.querySelector('.toggle_btn');
-const sidebar = document.querySelector('#sidebar');
+const closeBtn = document.querySelector('#btn-menu-close');
+const openBtn = document.querySelector('#btn-menu-open');
+const sidebar = document.querySelector('#sidebar-container');
 const main = document.querySelector('#main');
-toggle.addEventListener("click",function(){
-    toggleMove();
+
+closeBtn.addEventListener("click", function() {
+    sidebar.hidden = !sidebar.hidden;
+    openBtn.hidden = !openBtn.hidden;
 });
 
-function toggleMove() {
-    sidebar.classList.toggle('active');
-    // main.classList.toggle('resaze')
-}
+openBtn.addEventListener("click", function() {
+    sidebar.hidden = !sidebar.hidden;
+    openBtn.hidden = !openBtn.hidden;
+});
